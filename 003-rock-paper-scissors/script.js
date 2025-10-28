936e0167710592ac4eb01b66762fde35a2488c1b
vinceopaul <82008765+vinceopaul@users.noreply.github.com>
2025-10-28 21:35:33 +0800

    feat(comp): add function to get computer's choice
    Set a variable that randomly generates a number from 0 and 2,
    representing rock, paper, and scissors. Return its choice.

--- 003-rock-paper-scissors/script.js
+++ 003-rock-paper-scissors/script.js
@@ -1,4 +1,22 @@
 //Rock Paper Scissors
+function getComputerChoice() {
+  const randVal = Math.round(Math.random() * 2);
+  let choice = "";
+
+  switch (randVal) {
+    case 0:
+      choice = "rock";
+      break;
+    case 1:
+      choice = "paper";
+      break;
+    case 2:
+      choice = "scissors";
+      break;
+  }
+  return choice;
+}
+
 function getPlayerChoice() {
   const playerInput = prompt(
     "Hello Player! Input:\n1 - Rock\n2 - Paper\n3 - Scissors"
@@ -20,4 +38,5 @@ function getPlayerChoice() {
   return choice;
 }
 
-alert(getPlayerChoice());
+console.log("player:", getPlayerChoice());
+console.log("comp:", getComputerChoice());
