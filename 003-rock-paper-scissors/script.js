d0a28797657c0db6e96a1d409ed98d7517cadfd2
vinceopaul <82008765+vinceopaul@users.noreply.github.com>
2025-10-28 20:57:37 +0800

    feat(player): add function to get player's choice
    Ask the player to choose a number between 1 and 3
    representing rock, paper, and scissors. Return their choice.

--- 003-rock-paper-scissors/script.js
+++ 003-rock-paper-scissors/script.js
@@ -1,1 +1,23 @@
-console.log("Hello, World!");
+//Rock Paper Scissors
+function getPlayerChoice() {
+  const playerInput = prompt(
+    "Hello Player! Input:\n1 - Rock\n2 - Paper\n3 - Scissors"
+  );
+  let choice = "";
+
+  switch (playerInput) {
+    case "1":
+      choice = "rock";
+      break;
+    case "2":
+      choice = "paper";
+      break;
+    case "3":
+      choice = "scissors";
+      break;
+  }
+
+  return choice;
+}
+
+alert(getPlayerChoice());
