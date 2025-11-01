929408fc8a6014860802156397518d060f4f6b26
vinceopaul <82008765+vinceopaul@users.noreply.github.com>
2025-11-01 11:49:38 +0800

    feat(playGame): add functions that plays the game
    -Create a function playGame
    -Move score tracking, playRound, player and computer return choices,
    and playRound function call.
    
    These will make everything controlled, and resets the values
    every time the playGame function is called.

--- 003-rock-paper-scissors/script.js
+++ 003-rock-paper-scissors/script.js
@@ -12,33 +12,37 @@ function getPlayerChoice() {
   return playerChoice.toLowerCase();
 }
 
-let playerScore = 0;
-let computerScore = 0;
+function playGame() {
+  let playerScore = 0;
+  let computerScore = 0;
 
-function playRound(playerChoice, computerChoice) {
-  const beats = {
-    rock: "scissors",
-    paper: "rock",
-    scissors: "paper",
-  };
-  if (playerChoice === computerChoice) console.log("It's a tie! Play Again!");
-  else if (computerChoice === beats[playerChoice]) {
-    console.log(`You win! ${playerChoice} wins ${computerChoice}`);
-    playerScore += 1;
-  } else {
-    console.log(`You lose! ${computerChoice} wins ${playerChoice}`);
-    computerScore += 1;
+  function playRound(playerChoice, computerChoice) {
+    const beats = {
+      rock: "scissors",
+      paper: "rock",
+      scissors: "paper",
+    };
+    if (playerChoice === computerChoice) console.log("It's a tie! Play Again!");
+    else if (computerChoice === beats[playerChoice]) {
+      console.log(`You win! ${playerChoice} wins ${computerChoice}`);
+      playerScore += 1;
+    } else {
+      console.log(`You lose! ${computerChoice} wins ${playerChoice}`);
+      computerScore += 1;
+    }
   }
+
+  const playerSelection = getPlayerChoice();
+  const computerSelection = getComputerChoice();
+
+  console.log(playerSelection);
+  console.log(computerSelection);
+
+  playRound(playerSelection, computerSelection);
+
+  //Score Test
+  console.log(playerScore);
+  console.log(computerScore);
 }
 
-const playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice();
-
-console.log(playerSelection);
-console.log(computerSelection);
-
-playRound(playerSelection, computerSelection);
-
-//Score Test
-console.log(playerScore);
-console.log(computerScore);
+playGame();
