acf2ad5f9c6e497826a40971a40bcd0e324f685a
Paul Orceo <82008765+vinceopaul@users.noreply.github.com>
2025-12-22 10:19:11 +0800

    feat: implement generation of grid
    Add a function that handles the calculation of grid size and
    square size. Then use grid size and square size to create a grid.

---
+++ 004-etch-a-sketch/js/script.js
@@ -0,0 +1,34 @@
+function createGrid(gridSize, cellSize) {
+  const arrOfCell = [];
+
+  for (let i = 1; i <= gridSize; i++) {
+    const squareCell = document.createElement("div");
+    squareCell.className = "cell";
+    squareCell.style.flexBasis = `${cellSize}%`;
+
+    arrOfCell.push(squareCell);
+  }
+
+  return arrOfCell;
+}
+
+const calcGridSize = (cellCountPerSide) =>
+  (cellCountPerSide *= cellCountPerSide);
+
+const calcSquareSize = (cellCountPerSide) => 100 / cellCountPerSide;
+
+function startGrid(cellCountPerSide) {
+  const gridSize = calcGridSize(cellCountPerSide);
+  const squareCellSize = calcSquareSize(cellCountPerSide);
+  const grid = createGrid(gridSize, squareCellSize);
+
+  return grid;
+}
+
+function start(cellCountPerSide = 16 /* Default */) {
+  const gridData = startGrid(cellCountPerSide);
+}
+
+let cellCountPerSide;
+
+start(cellCountPerSide);
